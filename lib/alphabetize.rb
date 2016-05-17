a76b9bd764a95258d992db4a80884bca100b3531
eaharonoff <emanuel.aharonoff@gmail.com>
2016-05-17 09:40:10 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+   alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".each_char.with_index.to_h
+   arr.sort_by! do |str|
+    str.chars.map do |char| 
+      alpha[char]
+    end
+  end
 end
